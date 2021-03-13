304dc311405098f8b925b223bf450433c487c1cf
Angstylemon <maxd.gurr@gmail.com>
2021-03-13 15:46:16 +1300

    Setup router with login root + error page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+import LoginView from './pages/loginView';
+import PageNotFoundView from './pages/pageNotFoundView';
+
 function App() {
-	return <div className="app">
-		<h1>Hello</h1>
-	</div>;
+	return (
+		<Router>
+			<div className="app">
+				<h1>Global content</h1>
+				<Switch>
+					<Route exact path="/">
+						<LoginView />
+					</Route>
+					<Route>
+						<PageNotFoundView />
+					</Route>
+				</Switch>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
---
+++ src/pages/loginView.js
@@ -0,0 +1,7 @@
+export default function LoginView( ) {
+    return (
+        <div>
+            <h2>Login</h2>
+        </div>
+    );
+}
