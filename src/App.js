574fdf299fb2ecd75c81951f11b7e2ac250e9d85
Angstylemon <maxd.gurr@gmail.com>
2021-03-13 16:13:46 +1300

    Initial project setup (#19)
    * Initialised react app
    
    Co-Authored-By: Tushar Thakur <thakur.tushar0205@gmail.com>
    
    * Removed unnecessary files
    
    Co-Authored-By: Tushar Thakur <thakur.tushar0205@gmail.com>
    
    * Folder hierarchy, scss setup (#7)
    
    * Router setup (#18)
    
    * Initial project setup (#3)
    
    * Initialised react app
    
    Co-Authored-By: Tushar Thakur <thakur.tushar0205@gmail.com>
    
    * Removed unnecessary files
    
    Co-Authored-By: Tushar Thakur <thakur.tushar0205@gmail.com>
    
    Co-authored-by: Tushar Thakur <thakur.tushar0205@gmail.com>
    
    * Folder hierarchy, scss setup
    
    * Setup router with login root + error page
    
    * Remove unused files
    
    Co-authored-by: yuno99825 <49044003+yuno99825@users.noreply.github.com>
    Co-authored-by: Tushar Thakur <thakur.tushar0205@gmail.com>
    
    Co-authored-by: yuno99825 <49044003+yuno99825@users.noreply.github.com>
    Co-authored-by: Tushar Thakur <thakur.tushar0205@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import './App.css';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import LoginView from './pages/loginView';
+import PageNotFoundView from './pages/pageNotFoundView';
 
 function App() {
-	return <></>;
+	return (
+		<Router>
+			<div className="app">
+				<h1>Global content</h1>
+				<Switch>
+					{/* Route to login page */}
+					<Route exact path="/">
+						<LoginView />
+					</Route>
+
+					{/* Fallback - if none of the above routes are hit */}
+					<Route>
+						<PageNotFoundView />
+					</Route>
+				</Switch>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
