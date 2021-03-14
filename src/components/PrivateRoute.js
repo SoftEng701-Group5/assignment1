d469795411586b6f22d0826177cbb73ebb6b4845
yungikim1999 <danielkim1201@gmail.com>
2021-03-14 22:22:05 +1300

    Added "PrivateRoute" component.

---
+++ src/components/PrivateRoute.js
@@ -0,0 +1,23 @@
+import React, { useContext } from "react";
+import { Route, Redirect } from "react-router-dom";
+import { AuthContext } from "./Auth";
+
+const PrivateRoute = ({ component: RouteComponent, ...rest }) => {
+  const {currentUser} = useContext(AuthContext);
+  return (
+    <Route
+      {...rest}
+      render={routeProps =>
+        !!currentUser ? (
+          <RouteComponent {...routeProps} />
+        ) : (
+            // TODO: Change to usher the user back to login page
+          <Redirect to={"/login"} />
+        )
+      }
+    />
+  );
+};
+
+
+export default PrivateRoute
