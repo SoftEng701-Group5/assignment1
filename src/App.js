4e0f776fed4a94b6b124e33b59504c861ed9a8b3
kennyt12237 <kennyt12237@gmail.com>
2021-03-14 18:45:26 +1300

    Created TimeModal and the rightside

--- src/App.js
+++ src/App.js
@@ -1,33 +1,36 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import LoginView from "./pages/loginView";
-import HomeView from "./pages/homeView";
-import PageNotFoundView from "./pages/pageNotFoundView";
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import LoginView from './pages/loginView';
+import PageNotFoundView from './pages/pageNotFoundView';
+import TimerModal from './TimerModal/TimerModal';
+import TimerRightSide from "./TimerModal/TimerRightSide";
 
 function App() {
-  return (
-    <Router>
-      <div className="background">
-        <div className="main-content">
-          <Switch>
-            {/* Route to login page */}
-            <Route exact path="/">
-              <LoginView />
-            </Route>
-            <Route exact path="/home">
-              <HomeView />
-            </Route>
-            {/* Fallback - if none of the above routes are hit */}
-            <Route>
-              <PageNotFoundView />
-            </Route>
-          </Switch>
-        </div>
+    return (
+        <Router>
+            <div className="app">
+                <h1>Global content</h1>
+                <Switch>
+                    {/* Route to login page */}
+                    <Route exact path="/">
+                        <LoginView/>
+                    </Route>
 
-		<div className="circle1" />
-        <div className="circle2" />
-      </div>
-    </Router>
-  );
+                    <Route path={"/timerModal"}>
+                        <TimerModal onPlay={() => console.log("Play Pressed")}
+                                    onResize={() => console.log("Resize Pressed")}
+                                    children={TimerRightSide()}/>
+                    </Route>
+
+                    {/* Fallback - if none of the above routes are hit */}
+                    <Route>
+                        <PageNotFoundView/>
+                    </Route>
+                </Switch>
+                <div className="circle1" />
+                <div className="circle2" />
+            </div>
+        </Router>
+    );
 }
 
 export default App;
