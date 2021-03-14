ab8b54eb1c25e8f85d7962a62e608d9a29e14dd7
M-Stable <fchanghlee@gmail.com>
2021-03-14 17:49:45 +1300

    Add global styling, home page, and global button

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import LoginView from './pages/loginView';
-import PageNotFoundView from './pages/pageNotFoundView';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import LoginView from "./pages/loginView";
+import HomeView from "./pages/homeView";
+import PageNotFoundView from "./pages/pageNotFoundView";
 
 function App() {
-	return (
-		<Router>
-			<div className="app">
-				<h1>Global content</h1>
-				<Switch>
-					{/* Route to login page */}
-					<Route exact path="/">
-						<LoginView />
-					</Route>
+  return (
+    <Router>
+      <div className="background">
+        <div className="main-content">
+          <Switch>
+            {/* Route to login page */}
+            <Route exact path="/">
+              <LoginView />
+            </Route>
+            <Route exact path="/home">
+              <HomeView />
+            </Route>
+            {/* Fallback - if none of the above routes are hit */}
+            <Route>
+              <PageNotFoundView />
+            </Route>
+          </Switch>
+        </div>
 
-					{/* Fallback - if none of the above routes are hit */}
-					<Route>
-						<PageNotFoundView />
-					</Route>
-				</Switch>
-			</div>
-		</Router>
-	);
+		<div className="circle1" />
+        <div className="circle2" />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
