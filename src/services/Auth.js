70386ddd16b94cd43e9a7b19495d5c372d95e14c
dongmeilim <dongmeilim@gmail.com>
2021-03-14 22:26:54 +1300

    create AuthProvider.js

---
+++ src/services/Auth.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+import app from "./Firebase.js";
+
+export const AuthContext = React.createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(null);
+
+  useEffect(() => {
+    app.auth().onAuthStateChanged((user) => {
+      setCurrentUser(user)
+    });
+  }, []);
+
+
+  return (
+    <AuthContext.Provider
+      value={{
+        currentUser
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
