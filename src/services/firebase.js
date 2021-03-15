aea374b776a81ae38322eaa87dfb4377422bd4a4
yungikim1999 <danielkim1201@gmail.com>
2021-03-16 00:14:27 +1300

    firebase authorization methods added

---
+++ src/services/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase'
+require('firebase/auth')
+
+/**
+ * Initialize Firebase application with parameters
+ */
+const firebaseApp = firebase.initializeApp({
+    apiKey: "AIzaSyBziUug99HMl-cwPOzWnwHnzJxH_BmG-N8",
+    authDomain: "se701-group-5-assignment-1.firebaseapp.com",
+    projectId: "se701-group-5-assignment-1",
+    storageBucket: "se701-group-5-assignment-1.appspot.com",
+    messagingSenderId: "357803904307",
+});
+
+export default firebaseApp;
