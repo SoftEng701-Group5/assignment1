b3f6b60c6239930b79e65570899897914756e715
jeva126 <jeva126@aucklanduni.ac.nz>
2021-03-16 10:19:04 +1300

    Added left side components of the timer modal. Current Task and Notes sections

---
+++ src/components/TimerModal/LeftSideNotes.js
@@ -0,0 +1,13 @@
+export default function LeftSideNotes({notes}) {
+
+    return (
+        <div style={{margin: '10px', backgroundColor: 'navy'}}>
+            <h2>Notes</h2>
+            <div>
+
+                Testing notes<br/>testing notes
+
+            </div>
+        </div>
+        );
+}
--- src/components/TimerModal/TimerModal.js
+++ src/components/TimerModal/TimerModal.js
@@ -2,15 +2,22 @@ import ReactDOM from 'react-dom';
 import style from './Modal.module.css';
 const modalRoot = document.querySelector('#modal-root');
 
-export default function Modal({onPlay, onResize, children }) {
+export default function Modal({onPlay, onResize, children, leftSide }) {
 
     return ReactDOM.createPortal(
         <div className={style.modalContainer}>
 
             {/*Components are displayed here*/}
+            <div >
+                {leftSide}
+            </div>
+
             <div>
                 {children}
             </div>
+
+            
+
         </div>
         , modalRoot
     );
