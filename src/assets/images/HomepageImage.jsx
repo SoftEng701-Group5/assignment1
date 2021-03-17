407fde8b23770ef1cfa821cc76b866cfa76c5e69
Lucas Gao <lucas2005gao@gmail.com>
2021-03-18 01:12:36 +1300

    fix more linting errors

--- src/assets/images/HomepageImage.jsx
+++ src/assets/images/HomepageImage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+// eslint-disable-next-line no-unused-vars
 function HomepageImage(props) {
   return (
     <svg
@@ -18,9 +19,9 @@ function HomepageImage(props) {
           y2="287.56"
           gradientUnits="userSpaceOnUse"
         >
-          <stop offset="0" stop-color="gray" stop-opacity="0.25" />
-          <stop offset="0.54" stop-color="gray" stop-opacity="0.12" />
-          <stop offset="1" stop-color="gray" stop-opacity="0.1" />
+          <stop offset="0" stopColor="gray" stopOpacity="0.25" />
+          <stop offset="0.54" stopColor="gray" stopOpacity="0.12" />
+          <stop offset="1" stopColor="gray" stopOpacity="0.1" />
         </linearGradient>
       </defs>
       <title>studying</title>
--- src/assets/images/SignupImage.jsx
+++ src/assets/images/SignupImage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+// eslint-disable-next-line no-unused-vars
 function SignupImage(props) {
   return (
     <svg
