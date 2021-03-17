a6d43be2d688b66b6b4aeacf9ace4e8086ef26e2
Jesse Zeng <jzen349@aucklanduni.ac.nz>
2021-03-17 21:55:54 +1300

    Login page

--- src/pages/loginView.js
+++ src/pages/loginView.js
@@ -1,7 +1,33 @@
-export default function LoginView( ) {
-    return (
-        <div>
-            <h2>Login</h2>
-        </div>
-    );
+import React, {useState} from 'react';
+import Button from '../components/global/Button';
+import LoginField from '../components/LoginField';
+
+import '../stylesheets/loginView.scss';
+
+export default function LoginView() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const login = function () {
+    console.log(email, password);
+  };
+
+  return (
+    <div className='login_layout'>
+      <h1 className='welcome'>Welcome</h1>
+      <div className='email_container'>
+        <LoginField label='Email:' onChangeHandler={setEmail} type='email' />
+      </div>
+      <div className='password_container'>
+        <LoginField
+          label='Password:'
+          onChangeHandler={setPassword}
+          type='password'
+        />
+      </div>
+      <div onClick={login}>
+        <Button icon={'rightArrow'} text={'Login'} />
+      </div>
+      Don't have an account? <a href='/home'>Sign Up here</a>
+    </div>
+  );
 }
