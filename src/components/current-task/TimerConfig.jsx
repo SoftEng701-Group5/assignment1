f57a5df4ef7286134e3704d5fb2819c6472938a6
yuno99825 <49044003+yuno99825@users.noreply.github.com>
2021-03-18 16:46:57 +1300

    Make auto fullscreen checkbox
    Co-Authored-By: Tushar Thakur <thakur.tushar0205@gmail.com>
    Co-Authored-By: Francis <52264629+M-Stable@users.noreply.github.com>

--- src/components/current-task/TimerConfig.jsx
+++ src/components/current-task/TimerConfig.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TextInput from '../global/TextInput';
 
 const IntervalInput = (props) => {
 	const { textValue, title } = props;
+
 	return (
 		<div>
 			{title}
@@ -20,10 +21,28 @@ const IntervalInput = (props) => {
 };
 
 function TimerConfig() {
+	const [isChecked, setIsChecked] = useState(false);
+
+	const handleCheckBoxClick = () => setIsChecked(!isChecked);
+
 	return (
 		<div className='timer-config'>
 			<IntervalInput title='Work Interval' textValue='25' />
 			<IntervalInput title='Break Interval' textValue='05' />
+			<div className='timer-config__fullscreen-field'>
+				<div
+					className={`timer-config__checkbox${
+						isChecked ? '--checked' : ''
+					}`}
+					onClick={handleCheckBoxClick}
+					onKeyDown={handleCheckBoxClick}
+					role='checkbox'
+					aria-label='checkbox'
+					tabIndex='0'
+					aria-checked={isChecked}
+				/>
+				Auto Fullscreen
+			</div>
 		</div>
 	);
 }
