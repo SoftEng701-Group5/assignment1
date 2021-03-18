2a9c74c92d9bcb0d2a0aa31d703368fc92b04900
kennyt12237 <kennyt12237@gmail.com>
2021-03-18 19:46:41 +1300

    Removed TimerBundle and placed in TimerSection

--- src/components/TimerModal/TimerSection.js
+++ src/components/TimerModal/TimerSection.js
@@ -1,14 +1,34 @@
 
-import React from "react";
+import React, { useEffect, useContext } from "react";
+import {TimerContext, PlayContext } from "./TimerContextProvider";
+import Timer from "./Timer";
+import TimerPlay from "./TimerPlay";
 import TimerResize from "./TimerResize";
-import TimerBundle from "./TimerBundle";
 
-export default function TimerSection(timerTitle) {
+export default function TimerSection(props) {
+
+    const {timerTitle} = props;
+    const [timer, setTimer] = useContext(TimerContext)
+    const [play, setPlay] = useContext(PlayContext)
+
+    useEffect(() => {
+        let interval = null;
+
+        if (play) {
+            interval = setTimeout(() => setTimer(timer.seconds !== 0 && {seconds: timer.seconds - 1}), 1000);
+        }
+
+        return () => clearTimeout(interval);
+    }, [play, timer, setTimer]);
 
     return (
+
         <div>
-            <TimerBundle timerTitle={timerTitle}/>
+            <h2> {timerTitle} </h2>
+            <Timer startTime={timer}/>
+            <TimerPlay play={play} setPlay={setPlay}/>
             <TimerResize/>
         </div>
     );
+
 }
