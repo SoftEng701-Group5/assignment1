dae47f0df75932efa4ae98a5c5b69def69f1c29b
Daniel Dowling <ddow731@UoA.auckland.ac.nz>
2021-03-18 15:44:36 +1300

    A few more tests, however, don't know if they actually work yet. Will test when at a better computer

--- src/pages/loginView.js
+++ src/pages/loginView.js
@@ -1,11 +1,7 @@
-<<<<<<< HEAD
-import Button from '../components/global/Button'
-export default function LoginView( ) {
-=======
+
 import React from "react";
 
 export default function LoginView() {
->>>>>>> 2d731fc18ecae2073e836bc982ade5923292e233
     return (
         <div>
             {/*<h2>Login</h2>*/}
