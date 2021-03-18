7699a1db29901b54c342554901b35e0d78fd559e
Angstylemon <maxd.gurr@gmail.com>
2021-03-18 14:58:06 +1300

    Button additions

--- src/components/global/Button.jsx
+++ src/components/global/Button.jsx
@@ -1,16 +1,42 @@
 import React from "react";
 import { Link } from "react-router-dom";
+
 import getIcon from "./componentFunctions";
 
 function Button(props) {
-  const {path, text, icon, height, fontSize} = props;
+  const { path, text, icon, height, fontSize, handleOnClick } = props;
+
+  const handleKeyDown = function(e) {
+    if (e.keyCode === 13) {
+      console.log("hit");
+    }
+  }
+
+  const button = (
+    <div
+      className="button-container"
+      style={{ height }}
+      onClick={handleOnClick}
+      role="button"
+      tabIndex={0}
+      onKeyDown={handleKeyDown}
+    >
+      <span className="button-text" style={{ fontSize }}>
+        {text}
+      </span>
+      {getIcon(icon)}
+    </div>
+  );
   return (
-    <Link to={path} className="button-link">
-      <div className="button-container" style={{height}}>
-        <span className="button-text" style={{fontSize}}>{text}</span>
-        {getIcon(icon)}
-      </div>
-    </Link>
+    <>
+      {path ? (
+        <Link to={path} className="button-link">
+          {button}
+        </Link>
+      ) : (
+        button
+      )}
+    </>
   );
 }
 
