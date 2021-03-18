a296ded2ae02d3152a361f486d22ca20591b394c
Lucas Gao <lucas2005gao@gmail.com>
2021-03-18 15:48:40 +1300

    disabled no props spreading

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,6 +23,7 @@ module.exports = {
   ],
   rules: {
     "react/prop-types": "off",
+    "react/jsx-props-no-spreading": "off",
     "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
     "jest/no-disabled-tests": "warn",
     "jest/no-focused-tests": "error",
