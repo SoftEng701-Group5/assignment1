755c885ec7880096df70ec8a939cf718793410c0
yungikim1999 <danielkim1201@gmail.com>
2021-03-18 19:12:51 +1300

    made the sign-up method add first and last name to user

--- src/services/authService.js
+++ src/services/authService.js
@@ -20,15 +20,27 @@ const signIn = async (email, password) => {
     }
 }
 /**
- * Creates a new user within the firebase application with the given email and password
+ * Signs the user up to the firebase application.
+ * After they have successfully created their account, their first and last names
+ * are added to the firestore db
  * @param {*} email The email of the user as a string
  * @param {*} password The password of the user as a string
+ * @param {*} first_name The first name of the user as a string
+ * @param {*} last_name The last name of the user as a string
  */
-const signUp = async (email, password) => {
+const signUp = async (email, password, first_name, last_name) => {
     try{
-        await firebaseConnection
+        await firebaseApp
         .auth()
-        .createUserWithEmailAndPassword(email, password);
+        .createUserWithEmailAndPassword(email, password).then(Credential => {
+            const db = firebaseApp.firestore();
+            db.collection("Users").add({
+                user_id: Credential.user.uid,
+                First_name: first_name,
+                Last_name: last_name 
+            });
+        });
+        
     }catch(error){
         alert(error);
     }
