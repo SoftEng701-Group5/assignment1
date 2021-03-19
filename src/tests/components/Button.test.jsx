42508bd5f9d8ea2ecc5d13ee528266637af31fc1
DanielDowling999 <62310275+DanielDowling999@users.noreply.github.com>
2021-03-20 12:57:36 +1300

    adding basic button tests

---
+++ src/tests/components/Button.test.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import {render, cleanup} from '@testing-library/react';
+import {BrowserRouter} from 'react-router-dom'
+import Button from '../../components/global/Button'
+import '@testing-library/jest-dom/extend-expect'
+
+afterEach(cleanup)
+
+test("Renders without crashing", () => {
+    const div = document.createElement("div");
+    render(<BrowserRouter><Button />, div </BrowserRouter>);
+})
+
+test("Renders button correctly", () => {
+    const{container} = render(<BrowserRouter><Button path="/home" text ="Click Me"  /></BrowserRouter>);
+    expect(container).toHaveTextContent("Click Me");
+})
