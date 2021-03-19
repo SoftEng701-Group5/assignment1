a570cbf71f33afe54086783dba60cacd4dadf8fc
ireb15 <57650854+ireb15@users.noreply.github.com>
2021-03-19 18:08:34 +1300

    Added placeholders for Today's Tasks and Stats to the Welcome Dashboard

--- src/pages/welcomeDashboard.js
+++ src/pages/welcomeDashboard.js
@@ -1,11 +1,14 @@
 import React from "react";
 import CurrentTask from "../components/current-task/CurrentTask";
+import DashboardPlaceholder from "../components/current-task/DashboardPlaceholder";
 import "../stylesheets/welcomeDashboard.scss";
 
 function welcomeDashboard() {
   return (
       <div className="row">
+        <DashboardPlaceholder heading="Today's Tasks"/>
         <CurrentTask />
+        <DashboardPlaceholder heading="Stats"/>
       </div>
   );
 }
