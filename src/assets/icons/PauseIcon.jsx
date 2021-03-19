21c4b9e34501d2df6328f655cc0dbbd10cb2766c
Francis <52264629+M-Stable@users.noreply.github.com>
2021-03-19 18:09:57 +1300

    Add play/pause icon (#95)
    * add play pause

---
+++ src/assets/icons/PauseIcon.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function PauseIcon() {
+    return (
+        <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+            viewBox="0 0 365 365" 
+            style={{ width: "50%", height: "50%", fill: "white" }}
+            >
+            <g>
+                <rect x="74.5" width="73" height="365" />
+                <rect x="217.5" width="73" height="365" />
+            </g>
+        </svg>
+
+    );
+}
+
+export default PauseIcon;
