9bfae7c5dd78b8999f92a174638510338b128d3c
kennyt12237 <42660433+kennyt12237@users.noreply.github.com>
2021-03-19 19:58:51 +1300

    Implemented break timer and fixed edge cases (timer is 00:00)

--- src/components/timer-modal/TimerData.js
+++ src/components/timer-modal/TimerData.js
@@ -1,6 +1,11 @@
 /* Default timer set to 25 minutes (1500 seconds) */
 export const defaultTimer = {
-    seconds:1500
+    seconds:2
 }
+
+export const defaultBreakTimer = {
+    seconds:2
+}
+
 export const defaultPlay = false
 
--- src/components/timer-modal/TimerSection.jsx
+++ src/components/timer-modal/TimerSection.jsx
@@ -1,18 +1,47 @@
 import React, { useEffect, useContext } from "react";
-import { TimerContext, PlayContext } from "./TimerContextProvider";
+import {
+  TimerContext,
+  PlayContext,
+  BreakTimerContext,
+} from "./TimerContextProvider";
 import IconButton from "../global/IconButton";
 
 export default function TimerSection() {
   const [timer, setTimer] = useContext(TimerContext);
   const [play, setPlay] = useContext(PlayContext);
+  const [breakTimer, setBreakTimer] = useContext(BreakTimerContext);
 
   const minutes = Math.floor(timer.seconds / 60);
   const seconds = timer.seconds % 60;
 
+  const breakMinutes = Math.floor(breakTimer.seconds / 60);
+  const breakSeconds = breakTimer.seconds % 60;
+
+  const timerFormat = (m,s) => {
+    let timerStr = "";
+
+    if (Number.isNaN(m) && Number.isNaN(s)) {
+      return "00:00";
+    }
+
+    if (m < 10) {
+        timerStr+= "0";
+    }
+    timerStr+= m;
+    timerStr+= ":";
+
+    if (s < 10) {
+      timerStr+= "0";
+    }
+    timerStr+= s;
+
+    return timerStr;
+  };
+
   useEffect(() => {
     let interval = null;
 
-    if (play) {
+    if (timer.seconds > 0 && play) {
       interval = setTimeout(
         () => setTimer(timer.seconds !== 0 && { seconds: timer.seconds - 1 }),
         1000
@@ -22,13 +51,26 @@ export default function TimerSection() {
     return () => clearTimeout(interval);
   }, [play, timer, setTimer]);
 
+  useEffect(() => {
+    let interval = null;
+
+    if (timer.seconds === 0 && play) {
+      interval = setTimeout(
+        () =>
+          setBreakTimer(
+            breakTimer.seconds !== 0 && { seconds: breakTimer.seconds - 1 }),
+        1000
+      );
+    }
+
+    return () => clearTimeout(interval);
+  }, [play, timer, breakTimer, setBreakTimer]);
+
   return (
     <div>
-      <div className="timerTitle">Work</div>
+      <div className="timerTitle"> {timer.seconds === 0 ? "Break" : "Work"} </div>
       <h1 className="timer-text">
-        {minutes < 10 && "0"}
-        {minutes}:{seconds < 10 && "0"}
-        {seconds}
+          {timer.seconds === 0 ? timerFormat(breakMinutes, breakSeconds): timerFormat(minutes, seconds)}
       </h1>
 
       <div className="button-align">
