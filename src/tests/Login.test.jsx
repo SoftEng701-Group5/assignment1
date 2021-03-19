7489bd3677e8cbec0a6af8af48d7b9f6043bc71c
DanielDowling999 <62310275+DanielDowling999@users.noreply.github.com>
2021-03-20 12:35:12 +1300

    adding slight changes, trying to fix login issues

--- src/tests/Login.test.jsx
+++ src/tests/Login.test.jsx
@@ -32,7 +32,7 @@ test("Correct Login", async() => {
     const email = 'ddow731@aucklanduni.ac.nz';
     const password = 'TestAccount';
     // renderWithRouter(<App />, {route: '/'});
-    <App/>
+    // <App/>
     userEvent.type(screen.getByTestId('email'), email);
     userEvent.type(screen.getByTestId('password'), password);
     expect(screen.getByText('ddow731@aucklanduni.ac.nz')).toBeInTheDocument();
