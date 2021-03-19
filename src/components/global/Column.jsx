678d4f3d9343149f25b413416a7427e9f3e982d9
TessaGush <63177740+TessaGush@users.noreply.github.com>
2021-03-19 16:44:22 +1300

    fix up pull request comments. Fixed up styling

--- src/components/global/Column.jsx
+++ src/components/global/Column.jsx
@@ -1,10 +1,7 @@
 import React from "react";
-// import { Link } from "react-router-dom";
 import "../../stylesheets/components/global/column.scss"
-import Subtask from "./Task/Subtask";
 import Task from "./Task/Task"
 
-// import getIcon from "./componentFunctions";
 
 function Column(props) {
 
@@ -12,18 +9,18 @@ function Column(props) {
 
   return (
       <div className="container">
+
         <h1 className="heading"> {heading} </h1>
+        
         <div className = "column">
+
           <Task name = "Assignment 1"/>
           <Task name = "Task 1"/>
           <Task name = "Task 2"/>
-          <Task name = "Task 3"/>
           <Task name = "Assignment 2"/>
-          <Task name = "Task 1" subtask = "task"/>
-          <Subtask name = "task"/>
+          <Task name = "Task 1" />
           <Task name = "Task 2"/>
           <Task name = "Task 3"/>
-          
           
         </div>
       </div>
