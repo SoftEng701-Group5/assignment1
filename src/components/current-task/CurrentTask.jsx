8b972fa93e4e1fc438905aab9e0bb4e296f16564
yuno99825 <49044003+yuno99825@users.noreply.github.com>
2021-03-20 17:17:32 +1300

    Feature dashboard use timer context (#130)
    * Create states in current task timer
    
    Co-Authored-By: Tushar Thakur <thakur.tushar0205@gmail.com>
    
    * Update CurrentTask.jsx
    
    Co-Authored-By: Tushar Thakur <thakur.tushar0205@gmail.com>
    
    Co-authored-by: Tushar Thakur <thakur.tushar0205@gmail.com>

--- src/components/current-task/CurrentTask.jsx
+++ src/components/current-task/CurrentTask.jsx
@@ -1,73 +1,79 @@
-import React, { useState, useContext } from "react";
-import IconButton from "../global/IconButton";
-import Button from "../global/Button";
-import Task from "../global/Task/Task";
-import CurrentTaskNotes from "./CurrentTaskNotes";
-import TimerConfig from "./TimerConfig";
-import CurrentTaskTimer from "./CurrentTaskTimer";
-import {
-	PlayContext,
-} from '../timer-modal/TimerContextProvider';
+import React, { useState } from 'react';
+import IconButton from '../global/IconButton';
+import Button from '../global/Button';
+import Task from '../global/Task/Task';
+import CurrentTaskNotes from './CurrentTaskNotes';
+import TimerConfig from './TimerConfig';
+import CurrentTaskTimer from './CurrentTaskTimer';
 
 function CurrentTask() {
-  const [showConfig, setShowConfig] = useState(false);
-  const [showTimer, setShowTimer] = useState(false);
-  const [play, setPlay] = useContext(PlayContext);
+	const [showConfig, setShowConfig] = useState(false);
+	const [showTimer, setShowTimer] = useState(false);
+	const [timerConfigValues, setTimerConfigValues] = useState({
+		workMinutes: 25,
+		workSeconds: 0,
+		breakMinutes: 5,
+		breakSeconds: 0,
+		autoFullScreen: false,
+	});
 
-  const handleConfigButtonClicked = () => {
-    setShowConfig(!showConfig);
-  };
+	const handleConfigButtonClicked = () => {
+		setShowConfig(!showConfig);
+	};
 
-  const handleStartButtonClicked = () => {
-    setShowTimer(true);
-	setShowConfig(false);
-	setPlay(!play);
-  };
+	const handleStartButtonClicked = () => {
+		setShowConfig(false);
+		setShowTimer(true);
+	};
 
-  return (
-    <div className="current-task">
-      <h1 className="current-task__title"> Current Task:</h1>
-      <div className="current-task__content">
-        {showConfig ? (
-          <TimerConfig />
-        ) : (
-          <div className="current-task__info">
-            <Task
-              expanded
-              name="Current task name is really really long"
-              subtasks={[
-                { id: 0, name: "subtask1" },
-                { id: 1, name: "subtask2" },
-              ]}
-            />
-            <CurrentTaskNotes
-              notes={[
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-                "Maecenas porttitor eget purus sit amet commodo. Ut non interdum mi. Donec tortor eros, luctus rutrum purus eget, ultricies fringilla enim.",
-              ]}
-            />
-          </div>
-        )}
+	return (
+		<div className='current-task'>
+			<h1 className='current-task__title'> Current Task:</h1>
+			<div className='current-task__content'>
+				{showConfig ? (
+					<TimerConfig setTimerConfigValues={setTimerConfigValues} />
+				) : (
+					<div className='current-task__info'>
+						<Task
+							expanded
+							name='Current task name is really really long'
+							subtasks={[
+								{ id: 0, name: 'subtask1' },
+								{ id: 1, name: 'subtask2' },
+							]}
+						/>
+						<CurrentTaskNotes
+							notes={[
+								'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+								'Maecenas porttitor eget purus sit amet commodo. Ut non interdum mi. Donec tortor eros, luctus rutrum purus eget, ultricies fringilla enim.',
+							]}
+						/>
+					</div>
+				)}
 
-        {showTimer ? (
-          <CurrentTaskTimer />
-        ) : (
-          <div className="current-task__buttons">
-            <Button
-              className="current-task__"
-              text="START"
-              height="3rem"
-              fontSize="1.2rem"
-              handleOnClick={handleStartButtonClicked}
-            />
-            <div>
-              <IconButton className="current-task__" icon="settings" onClick={handleConfigButtonClicked} />
-            </div>
-          </div>
-        )}
-      </div>
-    </div>
-  );
+				{showTimer ? (
+					<CurrentTaskTimer timerConfigValues={timerConfigValues} />
+				) : (
+					<div className='current-task__buttons'>
+						<Button
+							className='current-task__'
+							text='START'
+							height='3rem'
+							fontSize='1.2rem'
+							handleOnClick={handleStartButtonClicked}
+						/>
+						<div>
+							<IconButton
+								className='current-task__'
+								icon='settings'
+								onClick={handleConfigButtonClicked}
+							/>
+						</div>
+					</div>
+				)}
+			</div>
+		</div>
+	);
 }
 
 export default CurrentTask;
--- src/components/current-task/TimerConfig.jsx
+++ src/components/current-task/TimerConfig.jsx
@@ -2,7 +2,12 @@ import React, { useState } from 'react';
 import TextInput from '../global/TextInput';
 
 const IntervalInput = (props) => {
-	const { textValue, title } = props;
+	const {
+		onMinutesChangeHandler,
+		onSecondsChangeHandler,
+		textValue,
+		title,
+	} = props;
 
 	return (
 		<div>
@@ -12,23 +17,70 @@ const IntervalInput = (props) => {
 					centered
 					className='timer-config__input-field'
 					textValue={textValue}
+					onChangeHandler={onMinutesChangeHandler}
 				/>
 				<span className='timer-config__input-separator'>:</span>
-				<TextInput centered textValue='00' />
+				<TextInput
+					centered
+					textValue='00'
+					onChangeHandler={onSecondsChangeHandler}
+				/>
 			</div>
 		</div>
 	);
 };
 
-function TimerConfig() {
+function TimerConfig(props) {
+	const { setTimerConfigValues } = props;
+
 	const [isChecked, setIsChecked] = useState(false);
 
-	const handleCheckBoxClick = () => setIsChecked(!isChecked);
+	const handleWorkMinutesChanged = (val) => {
+		setTimerConfigValues((prevState) => ({
+			...prevState,
+			workMinutes: parseInt(val, 10),
+		}));
+	};
+	const handleWorkSecondsChanged = (val) => {
+		setTimerConfigValues((prevState) => ({
+			...prevState,
+			workSeconds: parseInt(val, 10),
+		}));
+	};
+	const handleBreakMinutesChanged = (val) => {
+		setTimerConfigValues((prevState) => ({
+			...prevState,
+			breakMinutes: parseInt(val, 10),
+		}));
+	};
+	const handleBreakSecondsChanged = (val) => {
+		setTimerConfigValues((prevState) => ({
+			...prevState,
+			breakSeconds: parseInt(val, 10),
+		}));
+	};
+	const handleCheckBoxClick = () => {
+		setIsChecked(!isChecked);
+		setTimerConfigValues((prevState) => ({
+			...prevState,
+			autoFullScreen: isChecked,
+		}));
+	};
 
 	return (
 		<div className='timer-config'>
-			<IntervalInput title='Work Interval' textValue='25' />
-			<IntervalInput title='Break Interval' textValue='05' />
+			<IntervalInput
+				title='Work Interval'
+				textValue='25'
+				onMinutesChangeHandler={handleWorkMinutesChanged}
+				onSecondsChangeHandler={handleWorkSecondsChanged}
+			/>
+			<IntervalInput
+				title='Break Interval'
+				textValue='05'
+				onMinutesChangeHandler={handleBreakMinutesChanged}
+				onSecondsChangeHandler={handleBreakSecondsChanged}
+			/>
 			<div className='timer-config__fullscreen-field'>
 				<div
 					className={`timer-config__checkbox${
