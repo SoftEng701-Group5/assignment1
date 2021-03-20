f329081d20b5e9e1347553e3a184ea22f8c34cac
xinyi <xguo679@aucklanduni.ac.nz>
2021-03-20 17:45:29 +1300

    finish task List

--- src/components/global/TaskList.jsx
+++ src/components/global/TaskList.jsx
@@ -2,6 +2,7 @@
 import React, { useContext, useState, useEffect } from "react";
 import NewTask from "../NewTask";
 import Task from "./Task/Task";
+import "../../stylesheets/components/global/taskList.scss"
 
 import { fetchTasks } from "../../services/databaseService";
 import { AuthContext } from "../../services/providers/authProvider";
@@ -18,8 +19,9 @@ function TaskList() {
 
     
     return (
-        <div className="Task-List">
-            <div className="All-Tasks">
+        <div className="task-list">
+
+            <div className="all-tasks">
             {tasks.map((t) => (
                 <div key={t.Task_id}>
                     <Task name={t.Name} />
@@ -27,64 +29,14 @@ function TaskList() {
             ))}
             </div>
 
-            <div style={{display:"flex",flex:1}}>
+            <div className="add-task-button-container">
             <NewTask />
             </div>
         
         </div>
 
     )
-    // const [isExpanded, setIsExpanded] = useState(false);
-    // const handlePlusClick = () => setIsExpanded(!isExpanded);
-
-    // const [task, setTask] = React.useState("");
-    // const [tasks, setTasks] = React.useState([]);
-
-    // function handleTaskSubmit(e) {
-    //     e.preventDefault();
-    //     const newTask = {
-    //         id: Math.floor(Math.random() * 10000),
-    //         text: task,
-    //     };
-    //     const newTasks = [newTask, ...tasks];
-    //     setTasks(newTasks);
-    //     setTask("");
-    // }
-
-    // return (
-    //     <div>
-    //         <div> 
-    //         <h1 style={{textAlign: "center"}}>Today&apos;s Tasks</h1>
-    //         </div>
-
-    //         {tasks.map((t) => (
-    //             <div key={t.id} className="task">
-    //                 <Task name={t.text} />
-    //             </div>
-
-    //         ))}
-
-    //         <div className={`task${isExpanded ? "--expanded" : ""}`}>
-    //             <div className='task__header'>
-    //                 <PlusIcon handleOnClick={handlePlusClick} />
-    //             </div>
-
-    //             <div className={`task__content${isExpanded ? "--expanded" : ""}`}>
-    //                 <form>
-    //                     <input
-    //                         type='text'
-    //                         placeholder='Add a task name'
-    //                         onChange={(e) => setTask(e.target.value)}
-    //                         value={task}
-    //                     />
-    //                     {/* <button type="button" onClick={handleTaskSubmit}>Submit</button> */}
-    //                     <Button text="Add" handleOnClick={handleTaskSubmit} />
-    //                 </form>
-
-    //             </div>
-    //         </div>
-    //     </div>
-    // )
+    
 }
 
 export default TaskList;
