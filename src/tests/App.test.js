ec2192773770212426182f6471fcaa38b39ea31a
DanielDowling999 <62310275+DanielDowling999@users.noreply.github.com>
2021-03-20 17:26:53 +1300

    Edited a comment

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -2,10 +2,10 @@ import React from "react";
 import { render, screen } from '@testing-library/react';
 import App from '../App';
 
-// Basic test to check login page renders, will
+// Basic test to check login page renders, will add more once
+// tests are functional
 test("Login page renders", () =>{
     render(<App/>);
     const text = screen.getByText("Login");
     expect(text).toBeInTheDocument();
 })
-
