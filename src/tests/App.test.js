08dc0ca7c6b7118383f20da3aaaf67ff4b56cfa8
DanielDowling999 <62310275+DanielDowling999@users.noreply.github.com>
2021-03-20 13:49:47 +1300

    Added basic app test

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -2,8 +2,8 @@ import React from "react";
 import { render, screen } from '@testing-library/react';
 import App from '../App';
 
-test('renders learn react link', () => {
+test("Login page renders", () =>{
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+  const text = screen.getByText("Login");
+  expect(text).toBeInTheDocument();
+})
