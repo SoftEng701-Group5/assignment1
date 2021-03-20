a10042a4bd1fd655a2f4116977560dd6905e0893
DanielDowling999 <62310275+DanielDowling999@users.noreply.github.com>
2021-03-20 17:19:27 +1300

    Added data-testid to nav buttons, in preparation for nav tests

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,12 +23,14 @@ function Navbar() {
       <div>
         <Link to="/home" className="button-link">
           <div className="navbar__icon__group">
+            <strong data-testid="home"/>
             {location.pathname === "/home" && <div className="selected-box" />}
             <HomeIcon />
           </div>
         </Link>
         <Link to="/dashboard" className="button-link">
           <div className="navbar__icon__group">
+            <strong data-testid="dashboard"/>
             {location.pathname === "/dashboard" && (
               <div className="selected-box" />
             )}
@@ -37,6 +39,7 @@ function Navbar() {
         </Link>
         <Link to="/board" className="button-link">
           <div className="navbar__icon__group">
+            <strong data-testid="board"/>
             {location.pathname === "/board" && <div className="selected-box" />}
             <BoardIcon />
           </div>
@@ -56,6 +59,7 @@ function Navbar() {
             onKeyDown={signOutHandler}
             role="button" 
             tabIndex="0">
+            <strong data-testid="logout"/>
               <LogoutIcon />
           </div>
         </div>
