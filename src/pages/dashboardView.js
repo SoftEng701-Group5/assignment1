468872ea05678f5e3a500abc400ed262752a9397
yuno99825 <49044003+yuno99825@users.noreply.github.com>
2021-03-21 12:10:33 +1300

    add task list to dashboard page
    Co-Authored-By: Tushar Thakur <thakur.tushar0205@gmail.com>

--- src/pages/dashboardView.js
+++ src/pages/dashboardView.js
@@ -1,32 +1,33 @@
-import React from "react";
-import CurrentTask from "../components/current-task/CurrentTask";
-import Navbar from "../components/Navbar";
+import React from 'react';
+import CurrentTask from '../components/current-task/CurrentTask';
+import TaskList from '../components/global/TaskList';
+import Navbar from '../components/Navbar';
 
 const DashboardPlaceholder = (props) => {
-  const { title } = props;
-  return (
-    <div className="dashboard__placeholder-section">
-      <h1 className="dashboard__placeholder-title">{title}</h1>
-      <div className="dashboard__placeholder-content" />
-    </div>
-  );
+	const { title } = props;
+	return (
+		<div className='dashboard__placeholder-section'>
+			<h1 className='dashboard__placeholder-title'>{title}</h1>
+			<div className='dashboard__placeholder-content' />
+		</div>
+	);
 };
 
 function DashboardView() {
-  return (
-    <>
-      <Navbar />
-      <div className="dashboard">
-        <DashboardPlaceholder title="Today's Tasks:" />
-        <CurrentTask />
-        <div className="dashboard__placeholder-column">
-          <DashboardPlaceholder title="Stats:" />
-          <br />
-          <DashboardPlaceholder title="Goals:" />
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<Navbar />
+			<div className='dashboard'>
+				<TaskList />
+				<CurrentTask />
+				<div className='dashboard__placeholder-column'>
+					<DashboardPlaceholder title='Stats:' />
+					<br />
+					<DashboardPlaceholder title='Goals:' />
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default DashboardView;
