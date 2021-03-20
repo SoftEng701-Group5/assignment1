c48ef4b0d6032291f216b6575120629479e8a050
Lucas Gao <lucas2005gao@gmail.com>
2021-03-21 12:56:13 +1300

    running prettier on button.test.jsx

--- src/tests/components/Button.test.jsx
+++ src/tests/components/Button.test.jsx
@@ -1,14 +1,16 @@
 import React from "react";
-import {render, cleanup} from '@testing-library/react';
-import {BrowserRouter} from 'react-router-dom'
-import Button from '../../components/global/Button'
-import '@testing-library/jest-dom/extend-expect'
+import { render, cleanup } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import Button from "../../components/global/Button";
+import "@testing-library/jest-dom/extend-expect";
 
-afterEach(cleanup)
-
+afterEach(cleanup);
 
 test("Renders button correctly", () => {
-    const{container} = render(<BrowserRouter><Button path="/home" text ="Click Me"  /></BrowserRouter>);
-    expect(container).toHaveTextContent("Click Me");
-})
-
+  const { container } = render(
+    <BrowserRouter>
+      <Button path="/home" text="Click Me" />
+    </BrowserRouter>
+  );
+  expect(container).toHaveTextContent("Click Me");
+});
