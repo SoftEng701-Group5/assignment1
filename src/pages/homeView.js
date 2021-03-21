7e2e9d81844cf833b336818a37efccb4e2c81578
Francis <52264629+M-Stable@users.noreply.github.com>
2021-03-21 17:40:12 +1300

    add task list to home page and fix list css (#169)

--- src/pages/homeView.js
+++ src/pages/homeView.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
 import Button from "../components/global/Button";
 import HomepageImage from "../assets/images/HomepageImage";
 import DateTime from "../components/DateTime";
 import Navbar from "../components/Navbar";
+import { CurrentTaskContext } from "../components/timer-modal/TimerContextProvider";
+import { fetchTasks } from "../services/databaseService";
+import { AuthContext } from "../services/providers/authProvider";
+import TaskList from "../components/global/TaskList";
 
-function homeView() {
+function HomeView() {
+  const { currentUser } = useContext(AuthContext);
+  const [, setCurrentTask] = useContext(CurrentTaskContext);
+  const [tasks, setTasks] = useState([]);
+  const history = useHistory();
+
   const getGreeting = () => {
     const myDate = new Date();
     const hrs = myDate.getHours();
@@ -12,6 +22,17 @@ function homeView() {
     if (hrs < 12) return "Good Morning";
     if (hrs < 18) return "Good Afternoon";
     return "Good Evening";
+  };
+
+  useEffect(() => {
+    fetchTasks(currentUser.uid).then((res) => {
+      setTasks(res);
+    });
+  }, []);
+
+  const handleTaskClick = (task) => {
+    history.push("/dashboard");
+    setCurrentTask(task);
   };
 
   return (
@@ -22,15 +43,18 @@ function homeView() {
           <h1 className="home-page--welcome-message">
             <span>{getGreeting()}</span>
             <br />
-            <span>John</span>
+            <span>{currentUser?.firstName}</span>
           </h1>
           <Button
             icon="rightArrow"
             text="Dashboard"
-            height="5rem"
-            fontSize="2rem"
+            height="4rem"
+            fontSize="1.5rem"
             path="/dashboard"
           />
+        </div>
+        <div className="home-page--task-list">
+          <TaskList tasks={tasks} onTaskClick={handleTaskClick} />
         </div>
         <DateTime />
         <HomepageImage />
@@ -39,4 +63,4 @@ function homeView() {
   );
 }
 
-export default homeView;
+export default HomeView;
