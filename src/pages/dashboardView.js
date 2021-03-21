e340541196b86e8799fbe09316b1321f99a3bc61
yuno99825 <49044003+yuno99825@users.noreply.github.com>
2021-03-21 13:54:30 +1300

    Integration dashboard (#152)
    * add task list to dashboard page
    
    Co-Authored-By: Tushar Thakur <thakur.tushar0205@gmail.com>
    
    * style task list
    
    Co-Authored-By: Tushar Thakur <thakur.tushar0205@gmail.com>
    
    * style empty current task
    
    Co-Authored-By: Tushar Thakur <thakur.tushar0205@gmail.com>
    
    * get current task from props
    
    Co-Authored-By: Tushar Thakur <thakur.tushar0205@gmail.com>
    
    * Lift state of list of tasks to dashboard page
    
    Co-Authored-By: Tushar Thakur <thakur.tushar0205@gmail.com>
    
    * Add ability to select current task from task list
    
    Co-Authored-By: Tushar Thakur <thakur.tushar0205@gmail.com>
    
    * add box shadow to add button
    
    Co-Authored-By: Tushar Thakur <thakur.tushar0205@gmail.com>
    
    * style modal
    
    Co-Authored-By: Tushar Thakur <thakur.tushar0205@gmail.com>
    
    * use context for current task
    
    Co-Authored-By: Tushar Thakur <thakur.tushar0205@gmail.com>
    
    Co-authored-by: Tushar Thakur <thakur.tushar0205@gmail.com>

--- src/pages/dashboardView.js
+++ src/pages/dashboardView.js
@@ -1,32 +1,51 @@
-import React from "react";
-import CurrentTask from "../components/current-task/CurrentTask";
-import Navbar from "../components/Navbar";
+import React, { useContext, useState, useEffect } from 'react';
+import CurrentTask from '../components/current-task/CurrentTask';
+import TaskList from '../components/global/TaskList';
+import Navbar from '../components/Navbar';
+import { CurrentTaskContext } from '../components/timer-modal/TimerContextProvider';
+
+import { fetchTasks } from '../services/databaseService';
+import { AuthContext } from '../services/providers/authProvider';
 
 const DashboardPlaceholder = (props) => {
-  const { title } = props;
-  return (
-    <div className="dashboard__placeholder-section">
-      <h1 className="dashboard__placeholder-title">{title}</h1>
-      <div className="dashboard__placeholder-content" />
-    </div>
-  );
+	const { title } = props;
+	return (
+		<div className='dashboard__placeholder-section'>
+			<h1 className='dashboard__placeholder-title'>{title}</h1>
+			<div className='dashboard__placeholder-content' />
+		</div>
+	);
 };
 
 function DashboardView() {
-  return (
-    <>
-      <Navbar />
-      <div className="dashboard">
-        <DashboardPlaceholder title="Today's Tasks:" />
-        <CurrentTask />
-        <div className="dashboard__placeholder-column">
-          <DashboardPlaceholder title="Stats:" />
-          <br />
-          <DashboardPlaceholder title="Goals:" />
-        </div>
-      </div>
-    </>
-  );
+	const { currentUser } = useContext(AuthContext);
+	const [, setCurrentTask] = useContext(CurrentTaskContext);
+	const [tasks, setTasks] = useState([]);
+
+	const handleTaskClick = (task) => {
+		setCurrentTask(task);
+	};
+
+	useEffect(() => {
+		fetchTasks(currentUser.uid).then((res) => {
+			setTasks(res);
+		});
+	}, []);
+
+	return (
+		<>
+			<Navbar />
+			<div className='dashboard'>
+				<TaskList tasks={tasks} onTaskClick={handleTaskClick} />
+				<CurrentTask />
+				<div className='dashboard__placeholder-column'>
+					<DashboardPlaceholder title='Stats:' />
+					<br />
+					<DashboardPlaceholder title='Goals:' />
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default DashboardView;
