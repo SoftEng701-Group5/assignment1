eddef563a1ed15417e7bba7a5195219853152792
ireb15 <57650854+ireb15@users.noreply.github.com>
2021-03-24 19:34:43 +1300

    Formatting fixed

--- src/components/current-task/StatsGoalsPlaceholder.jsx
+++ src/components/current-task/StatsGoalsPlaceholder.jsx
@@ -2,15 +2,15 @@ import React from "react";
 import "../../stylesheets/components/current-task/StatsGoalsPlaceholder.scss";
 
 function StatsGoalsPlaceholder(props) {
-    const { headingStats, headingGoals } = props;
-    return (
-        <div className="container">
-            <h1 className="heading"> {headingStats} </h1>
-            <div className="placeholderStats" />
-            <h1 className="heading"> {headingGoals} </h1>
-            <div className="placeholderGoals" />
-        </div>
-    );
+  const { headingStats, headingGoals } = props;
+  return (
+    <div className="container">
+      <h1 className="heading"> {headingStats} </h1>
+      <div className="placeholderStats" />
+      <h1 className="heading"> {headingGoals} </h1>
+      <div className="placeholderGoals" />
+    </div>
+  );
 }
 
 export default StatsGoalsPlaceholder;
