0f838299c4ef822d6b75b91637f0570370ad7286
Sannan Hafeez <sannanhafeez06@gmail.com>
2021-04-05 13:33:46 +1200

    Move dark mode state to context

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import HomeView from "./pages/homeView";
 import BoardView from "./pages/boardView";
 import PageNotFoundView from "./pages/pageNotFoundView";
 import { AuthProvider } from "./services/providers/authProvider";
+import DarkModeContext from "./services/theme-context";
 import PrivateRoute from "./components/PrivateRoute";
 import DashboardView from "./pages/dashboardView";
 
@@ -30,35 +31,41 @@ function App() {
   return (
     <AuthProvider>
       <Router>
-        <div className={isDarkMode ? "background" : "background--light"}>
-          <div className="main-content">
-            <Switch>
-              {/* Login/Signup page - unrestricted access */}
-              <Route exact path="/">
-                <LoginView />
-              </Route>
-              <Route exact path="/signup">
-                <SignUpView />
-              </Route>
+        <DarkModeContext.Provider value={isDarkMode}>
+          <div className={isDarkMode ? "background" : "background--light"}>
+            <div className="main-content">
+              <Switch>
+                {/* Login/Signup page - unrestricted access */}
+                <Route exact path="/">
+                  <LoginView />
+                </Route>
+                <Route exact path="/signup">
+                  <SignUpView />
+                </Route>
 
-              {/* Restricted pages */}
-              <PrivateRoute component={HomeView} path="/home" exact />
+                {/* Restricted pages */}
+                <PrivateRoute component={HomeView} path="/home" exact />
 
-              {/* Trello Task View */}
-              <PrivateRoute component={BoardView} path="/board" exact />
+                {/* Trello Task View */}
+                <PrivateRoute component={BoardView} path="/board" exact />
 
-              {/* Welcome dashboard - navbar present */}
-              <PrivateRoute component={DashboardView} path="/dashboard" exact />
-              {/* Fallback - if none of the above routes are hit */}
-              <Route>
-                <PageNotFoundView />
-              </Route>
-            </Switch>
+                {/* Welcome dashboard - navbar present */}
+                <PrivateRoute
+                  component={DashboardView}
+                  path="/dashboard"
+                  exact
+                />
+                {/* Fallback - if none of the above routes are hit */}
+                <Route>
+                  <PageNotFoundView />
+                </Route>
+              </Switch>
+            </div>
+
+            <div className={isDarkMode ? "circle1" : "circle1 light"} />
+            <div className={isDarkMode ? "circle2" : "circle2 light"} />
           </div>
-
-          <div className={isDarkMode ? "circle1" : "circle1 light"} />
-          <div className={isDarkMode ? "circle2" : "circle2 light"} />
-        </div>
+        </DarkModeContext.Provider>
       </Router>
     </AuthProvider>
   );
---
+++ src/services/theme-context.js
@@ -0,0 +1,4 @@
+import React from "react";
+
+const DarkModeContext = React.createContext(true);
+export default DarkModeContext;
