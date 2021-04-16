24da93193c6178f680832fbe5638ea45d999c6b3
Charlie <charlieyang1040@hotmail.com>
2021-04-16 21:13:07 +1200

    added firebase services to fetch, update, and create goals

--- src/services/databaseService.js
+++ src/services/databaseService.js
@@ -82,4 +82,68 @@ const deleteTask = async (taskId) => {
   db.collection("Tasks").doc(taskId).delete();
 };
 
-export { fetchTasks, createTask, deleteTask, updateTask };
+/**
+ * A "get" request to the firebase cloud firestore which retrieves all the goals for a specific user
+ * @param {*} userId Unique user identification as a string. You can get this from currentUser.uid
+ * @returns All the specified users goals as an array of javascript objects with fields:
+ * Description: string
+ * End_date: timestamp
+ * Name: string
+ * Goal_id: string
+ * User_id: string
+ * Is_complete: boolean
+ */
+const fetchGoals = async (userId) => {
+  const db = firebaseConnection.firestore();
+  const data = await db
+    .collection("Goals")
+    .where("User_id", "==", userId)
+    .get();
+  const goals = data.docs.map((doc) => ({ ...doc.data(), Task_id: doc.id }));
+  return goals;
+};
+
+/**
+ * A "post" request to the firebase cloud firestore which creates an new goal for a specific user
+ * @param {*} description string
+ * @param {*} name string
+ * @param {*} userId string
+ * @param {*} endDate Date object
+ */
+const createGoal = async (description, name, userId, endDate) => {
+  const db = firebaseConnection.firestore();
+  db.collection("Goals").add({
+    Description: description,
+    Name: name,
+    User_id: userId,
+    End_date: endDate,
+    Is_complete: false,
+  });
+};
+
+/**
+ * A "put" request to the firebase cloud firestore which updates a specific goal for a specific user
+ * NOTE: param "newGoalData" must contain all fields of a goal, regardless of changes or not.
+ * @param {*} goalId ID of the goal as string
+ * @param {*} newTGoalData Javascript object containing goal data. These are:
+ * Description: string
+ * End_date: Date object
+ * Name: string
+ * Goal_id: string
+ * User_id: string
+ * Is_complete: boolean
+ */
+const updateGoal = async (goalId, newGoalData) => {
+  const db = firebaseConnection.firestore();
+  db.collection("Goals").doc(goalId).set(newGoalData);
+};
+
+export {
+  fetchTasks,
+  createTask,
+  deleteTask,
+  updateTask,
+  fetchGoals,
+  createGoal,
+  updateGoal,
+};
