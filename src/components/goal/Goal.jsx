5337372bb47a2abd02569cd6203e63c9c6cd2079
Chuppay <50004303+Chuppay@users.noreply.github.com>
2021-04-18 23:35:57 +1200

    Goals component (#265)
    * create goal components
    
    * added firebase services to fetch, update, and create goals
    
    * Finished component with firebase service
    
    * fix failed tests
    
    * fixed css consistency issues
    
    * fix small inconsistency issue

---
+++ src/components/goal/Goal.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import { updateGoal } from "../../services/databaseService";
+import DarkModeContext from "../../services/theme-context";
+
+function Goal(props) {
+  const {
+    name,
+    onClick,
+    selected,
+    endDate,
+    goalId,
+    checked,
+    description,
+    userId,
+  } = props;
+
+  const [isChecked, setIsChecked] = useState(checked);
+  const { isDarkMode } = React.useContext(DarkModeContext);
+
+  const handleCheckBoxClick = () => {
+    const newIsChecked = !isChecked;
+    setIsChecked(newIsChecked);
+    updateGoal(goalId, {
+      Description: description,
+      End_date: endDate,
+      Goal_id: goalId,
+      Is_complete: newIsChecked,
+      Name: name,
+      User_id: userId,
+    });
+  };
+
+  return (
+    <div className={`goal ${isDarkMode ? "" : "light"}`}>
+      <div className={`goal__header${selected ? "--selected" : ""}`}>
+        <div
+          className={`goal__checkbox${isChecked ? "--checked" : ""}`}
+          onClick={handleCheckBoxClick}
+          onKeyDown={handleCheckBoxClick}
+          role="checkbox"
+          aria-label="checkbox"
+          tabIndex="0"
+          aria-checked={isChecked}
+        />
+        <span
+          className={`goal__title${isChecked ? "--checked" : ""}`}
+          onClick={onClick}
+          onKeyDown={onClick}
+          role="button"
+          tabIndex="0"
+        >
+          {name}
+        </span>
+      </div>
+    </div>
+  );
+}
+export default Goal;
